a980555446cccf3ce4c0163a91786256b8a684cc
John Hamm <johndhamm1@gmail.com>
2017-06-07 00:19:53 -0500

    all react + redux installs upgraded, material-ui added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
 class App extends Component {
 
 	render() {
 		return (
-			<div>
-				<h2>App-tastic!</h2>
-			</div>
+			<MuiThemeProvider>
+				<div>
+					<h2 className="title-test">App-tastic!!</h2>
+					<RaisedButton label="RaisedButton" />
+				</div>
+			</MuiThemeProvider>
 		)
 	}
 }
