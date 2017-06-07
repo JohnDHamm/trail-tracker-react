450dd544be7e0aed90b83c4219d6728a24eae210
John Hamm <johndhamm1@gmail.com>
2017-06-07 17:32:47 -0500

    bootstrap4 added, data added to trails model

--- src/reducers/reducer_trails.js
+++ src/reducers/reducer_trails.js
@@ -4,17 +4,35 @@ export default function() {
 		1:
 		{ id: 1,
 			name: "Hamilton Creek",
-			location: "Nashville, TN"
+			location: "Nashville, TN",
+			description: "Located just 10 miles east of downtown Nashville, Hamilton Creek offers 2 trails totaling about 11 miles. Known for its' many rock features, this trail ranks as the most technical in the middle TN area.",
+			latitude: 36.101153,
+			longitude: -86.628356,
+			mapZoom: 15,
+			numOpenTickets: 1,
+			imgUrl: '/img/hamiltonCreek.jpg'
 		},
 		2:
 		{ id: 2,
 			name: "Percy Warner",
-			location: "Nashville, TN"
+			location: "Nashville, TN",
+			description: "Over 9 miles of singletrack in West Nashville with varying trail levels to accommodate all riders. Recently opened in 2014, these trails have been professionally built to the highest standards.",
+			latitude: 36.080692,
+			longitude: -86.878638,
+			mapZoom: 15,
+			numOpenTickets: 0,
+			imgUrl: '/img/percyWarner.jpg'
 		},
 		3:
 		{ id: 3,
 			name: "Lock 4",
-			location: "Gallatin, TN"
+			location: "Gallatin, TN",
+			description: "Lock 4 is located in Gallatin, TN, about 45 minutes from Nashville on the shores of Old Hickory Lake. This \"flow\" style trail consists of 9.3 miles of fast fun.",
+			latitude: 36.33062,
+			longitude: -86.470342,
+			mapZoom: 15,
+			numOpenTickets: 0,
+			imgUrl: '/img/lock4.jpg'
 		}
 	};
 
