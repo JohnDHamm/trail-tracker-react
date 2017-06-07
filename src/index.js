26211fc6d1269ba7534facd172efd6355fac7362
John Hamm <johndhamm1@gmail.com>
2017-06-06 23:55:44 -0500

    webpack1 setup + simple react app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+
+	render() {
+		return (
+			<div>
+				<h2>App-tastic!</h2>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
