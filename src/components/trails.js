ebf182e29aa373a78395b1f387400c55882c6b78
John Hamm <johndhamm1@gmail.com>
2017-06-13 22:54:23 -0500

    added open ticket badge to trails select card

--- src/components/trails.js
+++ src/components/trails.js
@@ -4,41 +4,45 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Navbar from './navbar'
-
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-// import {GridList, GridTile} from 'material-ui/GridList';
-// import FontIcon from 'material-ui/FontIcon';
-// import IconButton from 'material-ui/IconButton';
-// import Subheader from 'material-ui/Subheader';
-// import ActionInfoOutLine from 'material-ui/svg-icons/action/info-outline';
 import TrailsSelectCard from './trails_select_card';
 
-// const styles = {
-// 	root: {
-// 		display: 'flex',
-// 		flexWrap: 'wrap',
-// 		justifyContent: 'space-around'
-// 	},
-// 	gridList: {
-//     overflowY: 'auto'
-// 	},
-// };
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 
 class Trails extends Component {
 
 	renderTrails() {
 		return _.map(this.props.trails, trail => {
 			const trailURL = `/trail/${trail.id}`;
-			return (
-				<div className="col-lg-3 col-md-4 col-sm-6 col-8 offset-2 offset-sm-0" key={trail.id}>
-					<Link to={trailURL}>
-						<TrailsSelectCard
-							trailName={trail.name}
-							location={trail.location}
-							photoUrl={trail.imgUrl} />
-					</Link>
-				</div>
-			);
+
+			if (trail.numOpenTickets < 1) {
+				return (
+					<div className="col-lg-3 col-md-4 col-sm-6 col-8 offset-2 offset-sm-0" key={trail.id}>
+						<Link to={trailURL}>
+							<TrailsSelectCard
+								trailName={trail.name}
+								location={trail.location}
+								photoUrl={trail.imgUrl} />
+						</Link>
+					</div>
+				);
+			} else {
+				return (
+					<div className="col-lg-3 col-md-4 col-sm-6 col-8 offset-2 offset-sm-0" key={trail.id}>
+						<div className="trailWithTicketDiv">
+							<div className="ticketBadge">
+								{trail.numOpenTickets}
+							</div>
+							<Link to={trailURL}>
+								<TrailsSelectCard
+									trailName={trail.name}
+									location={trail.location}
+									photoUrl={trail.imgUrl} />
+							</Link>
+						</div>
+					</div>
+				);
+			}
 		});
 	}
 
