2ec65b2c0827260738303fc6b8df770145a73245
John Hamm <johndhamm1@gmail.com>
2017-06-16 09:51:44 -0500

    weather forecast api successful with backend

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import PostsReducer from './reducer_posts';
 import ValuesReducer from './reducer_values';
 import WeatherReducer from './reducer_weather';
 import CurrentWeatherReducer from './reducer_weather_current';
+import WeatherForecastReducer from './reducer_weather_forecast';
 
 const rootReducer = combineReducers({
   trails: TrailsReducer,
   posts: PostsReducer,
   values: ValuesReducer,
   currentWeather: CurrentWeatherReducer,
+  weatherForecast: WeatherForecastReducer,
   weather: WeatherReducer
 });
 
