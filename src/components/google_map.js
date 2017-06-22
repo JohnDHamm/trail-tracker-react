dc862e605ce04856e3c48e451d192008ac97112f
John Hamm <johndhamm1@gmail.com>
2017-06-21 19:28:22 -0500

    revised googleMap component for temp demo

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+function getStyles(props, context) {
+	const {GoogleMap} = context.muiTheme;
+	const {values} = props;
+
+	return {
+		root: {
+			position: 'relative'
+			// padding: 10
+		},
+		tempMsg: {
+			position: 'absolute',
+			top: 5,
+			left: 5,
+			fontSize: 25,
+			color: `${values.primary.color}`,
+			textShadow: `1px 1px 0 ${values.secondary.color}`,
+			fontFamily: `${values.tertiary.font}`,
+		},
+		coords: {
+			position: 'absolute',
+			bottom: 5,
+			left: 5,
+			fontSize: 15,
+			color: `${values.secondary.color}`,
+			textShadow: `1px 1px 0 ${values.primary.color}`,
+			fontFamily: `${values.tertiary.font}`,
+		}
+	};
+}
 
 class GoogleMap extends Component {
-
 	// componentDidMount() {
 	// 	new google.maps.Map(this.refs.map, {
 	// 		zoom: this.props.zoom,
@@ -11,16 +42,46 @@ class GoogleMap extends Component {
 	// 		}
 	// 	});
 	// }
+	static muiName = 'GoogleMap';
+
+	static propTypes = {
+		postUserName: PropTypes.node,
+		date: PropTypes.node,
+		message: PropTypes.node,
+		style: PropTypes.object,
+		userImgUrl: PropTypes.string
+	};
+
+	static contextTypes = {
+		muiTheme: PropTypes.object.isRequired,
+	};
 
 	render () {
 		//this.refs.map = html element to render map to
 		// return <div ref="map" />;
+		const { postUserName, date, message, style, userImgUrl } = this.props;
+		const {prepareStyles} = this.context.muiTheme;
+		const styles = getStyles(this.props, this.context);
+
 		return (
-			<div>
-				<img src="../src/img/hamiltonCreek_googleMap.jpg" width="100%" />
+			<div style={prepareStyles(Object.assign(styles.root, style))}>
+				<div>
+					<img src="../src/img/hamiltonCreek_googleMap.jpg" width="100%" />
+				</div>
+				<div style={prepareStyles(Object.assign(styles.tempMsg, style))}>
+					Placeholder for Google Map
+				</div>
+				<div style={prepareStyles(Object.assign(styles.coords, style))}>
+					Latitude: {this.props.currentTrail.latitude} Longitude: {this.props.currentTrail.longitude}
+				</div>
 			</div>
 		)
 	}
 }
 
-export default GoogleMap;
+function mapStateToProps({ currentTrail, values }) {
+	return { currentTrail, values };
+}
+
+// export default GoogleMap;
+export default connect(mapStateToProps)(GoogleMap);
