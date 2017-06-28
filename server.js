5ca8b592c2ca38ed8c55463b034e577392cb196a
John Hamm <johndhamm1@gmail.com>
2017-06-28 18:11:35 -0500

    server cleanup, deploy works

--- server.js
+++ server.js
@@ -2,39 +2,20 @@
 
 const express = require('express');
 const path = require('path');
-// const { json } = require('body-parser');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.set('port', PORT);
 
-
 //middlewares
 app.use(express.static(__dirname));
-// app.use(json());
-// Add headers
-// app.use(function (req, res, next) {
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     // Pass to next layer of middleware
-//     next();
-// });
 
 // API
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-
 app.listen(PORT, () => {
 	console.log(`Listening on port: ${PORT}`);
 });
-
