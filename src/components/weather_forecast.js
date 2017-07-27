8af746f3097dd675ee6466d1287366f9c5d3542e
John Hamm <johndhamm1@gmail.com>
2017-07-27 17:46:23 -0500

    changing how components are styled - continued

--- src/components/weather_forecast.js
+++ src/components/weather_forecast.js
@@ -1,91 +1,66 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 
-function getStyles(props, context) {
-	const {WeatherForecast} = context.muiTheme;
-	const {values} = props;
-
-	return {
-		root: {
-			width: '100%',
-		},
-		weekday: {
-			color: `${values.primary.color}`,
-			fontFamily: `${values.tertiary.font}`,
-			fontSize: 20,
-			display: 'flex',
-			justifyContent: 'space-between',
-			alignItems: 'flex-end'
-		},
-		conditions: {
-			fontSize: 14,
-			fontFamily: `${values.primary.font}`,
-			color: `${values.tertiary.color}`,
-			marginBottom: 5,
-			lineHeight: 1
-		},
-		temps: {
-			fontSize: 14,
-			lineHeight: 1.3
-		},
-		precip: {
-			fontSize: 12,
-			lineHeight: 1.2
-		},
-		humidity: {
-			fontSize: 12,
-			lineHeight: 1.2
-		}
-	};
-}
-
 class WeatherForecast extends Component {
-	static muiName = 'WeatherForecast';
-
-	static propTypes = {
-		style: PropTypes.object,
-		weekday: PropTypes.node,
-		conditions: PropTypes.node,
-		iconUrl: PropTypes.string,
-		tempHigh: PropTypes.node,
-		tempLow: PropTypes.node,
-		precip: PropTypes.node,
-		humidity: PropTypes.node
-	};
-
-	static contextTypes = {
-		muiTheme: PropTypes.object.isRequired,
-	};
 
 	render () {
-		const { style, weekday, conditions, iconUrl, tempHigh, tempLow, precip, humidity } = this.props;
-		const {prepareStyles} = this.context.muiTheme;
-		const styles = getStyles(this.props, this.context);
+		const { style, weekday, conditions, iconUrl, tempHigh, tempLow, precip, humidity, values } = this.props;
+		const styles = {
+			root: {
+				width: '100%',
+			},
+			weekday: {
+				color: `${values.primary.color}`,
+				fontFamily: `${values.tertiary.font}`,
+				fontSize: 20,
+				display: 'flex',
+				justifyContent: 'space-between',
+				alignItems: 'flex-end'
+			},
+			conditions: {
+				fontSize: 14,
+				fontFamily: `${values.primary.font}`,
+				color: `${values.tertiary.color}`,
+				marginBottom: 5,
+				lineHeight: 1
+			},
+			temps: {
+				fontSize: 14,
+				lineHeight: 1.3
+			},
+			precip: {
+				fontSize: 12,
+				lineHeight: 1.2
+			},
+			humidity: {
+				fontSize: 12,
+				lineHeight: 1.2
+			}
+		};
 
 		if (!weekday) {
 			return ( <div>loading forecast...</div>)
 		}
 
 		return (
-			<div style={prepareStyles(Object.assign(styles.root, style))}>
-				<div style={prepareStyles(Object.assign(styles.weekday, style))}>
+			<div style={styles.root}>
+				<div style={styles.weekday}>
 					<span>
 						{weekday}
 					</span>
 					<img src={iconUrl} width="35"></img>
 				</div>
-				<div style={prepareStyles(Object.assign(styles.conditions, style))}>
+				<div style={styles.conditions}>
 						{conditions}
 				</div>
-				<div style={prepareStyles(Object.assign(styles.temps, style))}>
+				<div style={styles.temps}>
 					High {tempHigh}&deg; / Low {tempLow}&deg;
 				</div>
-				<div style={prepareStyles(Object.assign(styles.humidity, style))}>
+				<div style={styles.humidity}>
 					Humidity {humidity}%
 				</div>
-				<div style={prepareStyles(Object.assign(styles.precip, style))}>
+				<div style={styles.precip}>
 					Precip. {precip}"
 				</div>
 			</div>
